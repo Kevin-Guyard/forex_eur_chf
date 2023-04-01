4bf5919913213edd1cf7cd74033e8f4ab2c8c104
Kévin Guyard <kevin.guyard@live.fr>
2023-04-02 00:40:44 +0200

    command slurm

---
+++ slurm/mlp_3_week_memory_y_ask.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --job-name mlp_3_week_memory_y_ask
+#SBATCH --error mlp_3_week_memory_y_ask-error.e%j
+#SBATCH --output mlp_3_week_memory_y_ask-out.o%j
+#SBATCH --partition shared-gpu
+#SBATCH --gpus=1
+#SBATCH --time 12:00:00
+#SBATCH --mem-per-cpu=10000
+
+module load GCCcore/11.2.0 Python/3.9.6
+module load GCCcore/10.2.0 CUDA/11.1.1
+
+pip install scikit-learn==1.0.2
+pip install numpy==1.21.5
+pip install pandas==1.4.4
+pip install optuna==3.1.0
+
+python main_tuning.py MLP3WeekMemory y_ask
