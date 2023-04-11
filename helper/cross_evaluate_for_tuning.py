6f1e85225dd25f1160f6be64da3e35a873d86c5a
Kévin Guyard <kevin.guyard@live.fr>
2023-04-11 12:48:01 +0200

    Add comment

--- helper/cross_evaluate_for_tuning.py
+++ helper/cross_evaluate_for_tuning.py
@@ -8,6 +8,7 @@ def cross_evaluate_for_tuning(model, dataset_trains, dataset_validations, optimi
     model_state_dict = deepcopy(model.state_dict())
     cross_val_scores = []
     
+    # Iterate over datasets and return the mean score
     for dataset_train, dataset_validation in zip(dataset_trains, dataset_validations):
         
         model.load_state_dict(model_state_dict)
